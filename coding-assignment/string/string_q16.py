d47bde8777040e835188c3e3845be03518f35232
rtkrawat <ritika.rawat_cs.h24@gla.ac.in>
2024-11-28 09:14:24 -0800

    Create string_q16.py

---
+++ coding-assignment/string/string_q16.py
@@ -0,0 +1,12 @@
+#16. Find the longest common prefix among an array of strings
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+    return prefix
+print(longest_common_prefix(["flower", "flow", "flight"]))
