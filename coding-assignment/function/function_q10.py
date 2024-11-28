91c076319d3ddf1c0c5a50f521d32b806f4de8a3
rtkrawat <ritika.rawat_cs.h24@gla.ac.in>
2024-11-28 08:16:09 -0800

    Create function_q10.py

---
+++ coding-assignment/function/function_q10.py
@@ -0,0 +1,30 @@
+# Geek is very fond of patterns. Once, his teacher gave him a  pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+# Help Geek to build a pattern
+
+"""
+Input: 5
+
+Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+
+"""
+
+
+def printTriangle(N):
+    for i in range(1, N + 1):
+        for j in range(i - 1):
+            print(" ", end="")
+        for j in range(N - i + 1):
+            print("*", end="")
+        for j in range(N - i):
+            print("*", end="")
+        print()
+
+
+printTriangle(5)
