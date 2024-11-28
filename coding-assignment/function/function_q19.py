9414e5126c480855468e7ecc27cc1edc27aac37c
rtkrawat <ritika.rawat_cs.h24@gla.ac.in>
2024-11-28 08:23:19 -0800

    Create function_q19.py

---
+++ coding-assignment/function/function_q19.py
@@ -0,0 +1,20 @@
+# Given two strings S1 and S2 as input, the task is to merge them alternatively i.e. the first character of S1 then the first character of S2 and so on till the strings end.
+# NOTE: Add the whole string if other string is empty.
+
+
+def merge(S1, S2):
+    p = ""
+    for i in range(len(S1) + len(S2)):
+        if i < len(S1) and i < len(S2):
+            p += S1[i]
+            p += S2[i]
+
+    if len(S1) > len(S2):
+        p += S1[len(S2) :]
+    elif len(S2) > len(S1):
+        p += S2[len(S1) :]
+
+    return p
+
+
+print(merge("make", "noice"))
